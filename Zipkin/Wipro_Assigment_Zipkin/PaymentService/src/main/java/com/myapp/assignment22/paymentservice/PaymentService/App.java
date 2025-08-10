81372ed6c5ab694abe4a62a256c7894f4ae1e021
Prajwal Atkare <prajwalatkare1@gmail.com>
2025-08-11 01:11:05 +0530

    addes

---
+++ Zipkin/Wipro_Assigment_Zipkin/PaymentService/src/main/java/com/myapp/assignment22/paymentservice/PaymentService/App.java
@@ -0,0 +1,13 @@
+package com.myapp.assignment22.paymentservice.PaymentService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Zipkin/Wipro_Assigment_Zipkin/Service_order_Assigmet/src/main/java/com/myapp/assignment22/orderservice/OrderService/App.java
@@ -0,0 +1,13 @@
+package com.myapp.assignment22.orderservice.OrderService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
